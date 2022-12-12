ee4a97131077a27241d5fc63aea92a20edcaa534
Robin Löseke <robin.loeseke@gmail.com>
2022-12-12 18:30:17 +0100

    created models.py for sensors app

--- base/sensors/models.py
+++ base/sensors/models.py
@@ -4,18 +4,33 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Sensor(models.Model):
+
+    class SensorTypes(models.TextChoices):
+        PRODUCTION_METER = 'PM', 'Production Meter'
+        CONSUMPTION_METER = 'CM', 'Consumption Meter'
+        GRID_METER = 'GM', 'Grid Meter'
+    # ID an der wir am Ende Sensor einer WE zuordnen können, wahrscheinlich ZählerNr oder so
     device_id = models.IntegerField()
-    eui_id = models.IntegerField()
+    type = models.CharField(
+        max_length=2,
+        choices=SensorTypes.choices
+    )
 
 
 class Producer(models.Model):
     name = models.CharField(max_length=30)
-    sensor = models.OneToOneField(Sensor, on_delete=models.CASCADE)
+    street = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=20)
+    peak_power = models.IntegerField()
+    production_sensor = models.OneToOneField(Sensor, on_delete=models.RESTRICT)
+    grid_sensor = models.OneToOneField(Sensor, on_delete=models.RESTRICT, related_name="grid_sensor")
 
 
 class Consumer(models.Model):
     name = models.CharField(max_length=30)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
     producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
-    sensor = models.OneToOneField(Sensor, on_delete=models.CASCADE)
+    sensor = models.OneToOneField(Sensor, on_delete=models.RESTRICT)

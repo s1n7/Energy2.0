31c698b2b475e6a8ca87f074dc30d543b16f4263
Alex Krawtschuk <teacheralexhia456@gmail.com>
2023-01-15 11:27:22 +0100

    Gross und Kleinschreibung korrigiert

--- base/views.py
+++ base/views.py
@@ -16,3 +16,25 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+
+# users/views.py
+from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+class LoginView(APIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # authenticate user here
+        user = authenticate(request, username=request.data.get('username'), password=request.data.get('password'))
+        if user:
+            login(request, user)
+            # return user data in response
+            return Response({'username': user.username})
+        else:
+            return Response({'error': 'Invalid login credentials'}, status=401)
